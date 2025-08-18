f0ac690ec9a87278a7d31d90712e48c9caf11622
SanthoshDevulapally <58581333+Santhoshdevulapallay@users.noreply.github.com>
2025-08-18 15:11:12 +0530

    Login disabled LoginForm.tsx

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -31,11 +31,11 @@ const LoginForm = ({ onLogin }: LoginFormProps) => {
       }
       setIsLoading(true);
       try {
-        const data = await loginUser(email, password);
-        toast({
-          title: "Welcome back!",
-          description: "Login successful",
-        });
+        // const data = await loginUser(email, password);
+        // toast({
+        //   title: "Welcome back!",
+        //   description: "Login successful",
+        // });
         onLogin(email, password);
         console.log("Access Token:", data.access);
       } catch (error: any) {
